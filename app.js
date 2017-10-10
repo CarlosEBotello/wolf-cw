0564f37cdc96d75f686abce915127269b2d49267
charlyWazzup <charlyw118@gmail.com>
2017-10-10 14:01:52 -0500

    Ya existe la logica base

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-console.log('Hola mundo!','fuck yeah');
+//console.log('Hola mundo!','fuck yeah');
 
-var nombre = "Charly";
-var nick = "CharlyWazzup";
+//var nombre = "Charly";
+//var nick = "CharlyWazzup";
 
-console.log(nombre,'es', nick);
+//console.log(nombre,'es', nick);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
